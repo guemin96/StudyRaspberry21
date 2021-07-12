e31b3283b8b57dfb532dbc69af592f86a9aae19a
guemin96 <guemin12@naver.com>
2021-07-12 15:32:56 +0900

    2021test

---
+++ test01.py
@@ -0,0 +1,33 @@
+n =1
+name = 'Park'
+n=n+1
+value = 1.2*n
+
+#print('{0} = 1.2*{1}'.format(value,n))
+#print(name)
+
+
+greeting = 'Hello world'
+#print(greeting[0])
+#print(greeting[2:7])
+
+#print(greeting *2)
+
+numbers = [0,1,2,3]
+print(numbers)
+print(numbers[3])
+
+names = ['Kim','Lee','Park','Choi']
+array = numbers+names
+# print(array)
+# print(array[-1])
+# print(array*2)
+# array[3] =7
+# print(array)
+
+#Tuple
+person = ('Kim', 24, 'male')
+print(person)
+print(person[1])
+name, age, gender = person
+print(age)
